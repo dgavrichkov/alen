619d6d65c34a1f7da74988c26398055a27d98a02
dgavrichkov <dgavrichkov32@gmail.com>
2021-05-19 13:54:58 +0300

    поддержит загрузку старых типов шрифтов галпом

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -45,9 +45,9 @@ const requireDir = require("require-dir"),
             watch: "./src/img/svg/*.svg"
         },
         fonts: {
-            src: "./src/fonts/**/*.{woff,woff2}",
+            src: "./src/fonts/**/*.{woff,woff2,otf,ttf}",
             dist: "./dist/fonts/",
-            watch: "./src/fonts/**/*.{woff,woff2}"
+            watch: "./src/fonts/**/*.{woff,woff2,otf,ttf}"
         },
         favicons: {
             src: "./src/img/favicon/*.{jpg,jpeg,png,gif,tiff}",
