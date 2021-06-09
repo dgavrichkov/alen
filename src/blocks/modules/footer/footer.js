4623aa91f957942bdd9402c5af90a2470d1b91ab
DitmarGrim <dgavrichkov32@gmail.com>
2021-06-09 15:34:33 +0300

    плавность аккордеона в футере

--- src/blocks/modules/footer/footer.js
+++ src/blocks/modules/footer/footer.js
@@ -2,7 +2,14 @@ const footerItemAccordeon = () => {
     const triggers = document.querySelectorAll(".footer__title");
     triggers.forEach(trg => {
         trg.addEventListener("click", function() {
-            trg.parentElement.classList.toggle("is-active");
+            if(!trg.parentElement.classList.contains("is-active")) {
+                trg.parentElement.classList.add("is-active");
+                trg.parentElement.style.height = trg.parentElement.scrollHeight + "px";
+            } else {
+                trg.parentElement.classList.remove("is-active");
+                trg.parentElement.style.height = "";
+            
+            }
         });
     });
 
