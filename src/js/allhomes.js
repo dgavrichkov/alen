d276b7daac06cb9f084f6a44da594bf536c9da37
dgavrichkov <dgavrichkov32@gmail.com>
2021-10-20 14:58:52 +0300

    частичная стилизация футера

--- src/js/allhomes.js
+++ src/js/allhomes.js
@@ -4,6 +4,7 @@ window.addEventListener("load", function() {
     loadAnimate();
     filterRangeBlocks();
     tripleSliderBlock();
+    footerAccordeon();
 });
 
 class Header {
@@ -500,5 +501,21 @@ const filterRangeBlocks = function() {
             'min': 0,
             'max': 100,
         }
+    });
+};
+
+const footerAccordeon = () => {
+    const triggers = document.querySelectorAll(".footer__title");
+    triggers.forEach(trg => {
+        trg.addEventListener("click", function () {
+            if (!trg.parentElement.classList.contains("is-active")) {
+                trg.parentElement.classList.add("is-active");
+                trg.parentElement.style.height = trg.parentElement.scrollHeight + "px";
+            } else {
+                trg.parentElement.classList.remove("is-active");
+                trg.parentElement.style.height = "";
+
+            }
+        });
     });
 };
